af013b0b89e75f0308c3a71df021b1911c62239e
D07Pwn3d <9zekoz9@gmail.com>
2023-01-28 18:55:31 +0100

    init

---
+++ client.py
@@ -0,0 +1,413 @@
+import json
+import socket
+import threading
+from typing import Dict, List, Optional, Tuple
+import re
+
+import pyaudio
+import tkinter as tk
+
+
+class Client(tk.Tk):
+    """A softphone client that uses tkinter for the GUI and pyaudio for audio recording and transmission.
+
+    This class handles the connection to the server, the GUI, and the audio transmission.
+    """
+
+    def __init__(self):
+        """Initializes the GUI and the sockets for the connection to the server and for the audio transmission."""
+        tk.Tk.__init__(self)
+        self.server_ip: Optional[str] = None
+        self.server_port: Optional[int] = None
+        self.client_name: Optional[str] = None
+        self.listening_for_calls = False
+        self.caller_ip = None
+        self.call_in_progress = False
+        self.connexion_serveur = False
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            self.udp_socket.bind(("", 5001))
+        except socket.error as e:
+            print(f"Error binding to port 5001 : {e}")
+            self.udp_socket.close()
+
+        # Initialize the pyaudio recording and playback objects.
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.FREQUENCE = 8000  # en Hertz
+        self.NB_ECHANTILLONS = 512
+        self.pyaudio = pyaudio.PyAudio()
+        self.input_stream = self.pyaudio.open(
+            format=self.FORMAT, channels=self.CHANNELS, rate=self.FREQUENCE, input=True
+        )
+        self.output_stream = self.pyaudio.open(
+            format=self.FORMAT, channels=self.CHANNELS, rate=self.FREQUENCE, output=True
+        )
+
+        # Create the GUI elements and set their position and behavior.
+        self.title("Client")
+        self.log_text = tk.Text(self, width=50, height=10)
+        self.log_text.pack()
+
+        self.config_frame = tk.Frame(self)
+        self.config_frame.pack()
+        self.server_ip_label = tk.Label(self.config_frame, text="Server IP:")
+        self.server_ip_label.pack(side="left")
+        self.server_ip_entry = tk.Entry(self.config_frame)
+        self.server_ip_entry.pack(side="left")
+        self.server_ip_entry.insert(0, "192.168.38.169")
+        self.server_port_label = tk.Label(self.config_frame, text="Server port:")
+        self.server_port_label.pack(side="left")
+        self.server_port_entry = tk.Entry(self.config_frame)
+        self.server_port_entry.pack(side="left")
+        self.server_port_entry.insert(0, "10000")
+        self.client_name_label = tk.Label(self.config_frame, text="Client name:")
+        self.client_name_label.pack(side="left")
+        self.client_name_entry = tk.Entry(self.config_frame)
+        self.client_name_entry.pack(side="left")
+        self.config_button = tk.Button(
+            self.config_frame, text="Configure", command=self.configure
+        )
+        self.config_button.pack(side="left")
+        self.bind("<Return>", self.configure)
+
+        self.call_frame = tk.Frame(self)
+        self.call_frame.pack()
+        self.call_name_label = tk.Label(self.call_frame, text="Call to:")
+        self.call_name_label.pack(side="left")
+        self.call_name_entry = tk.Entry(self.call_frame)
+        self.call_name_entry.pack(side="left")
+        self.call_button = tk.Button(
+            self.call_frame, text="Call", command=self.call, state=tk.DISABLED
+        )
+        self.call_button.pack(side="left")
+
+        self.btn_raccrocher = tk.Button(
+            self, text="Raccrocher", command=self.raccrocher, state=tk.DISABLED
+        )
+        self.btn_raccrocher.pack()
+
+        self.close_button = tk.Button(self, text="Close", command=self.close)
+        self.close_button.pack()
+
+        self.log_text.tag_config("avertissement", foreground="red")
+
+    def configure(self, *args) -> None:
+        """Configures the connection to the server using the information entered in the GUI."""
+        server_ip = self.server_ip_entry.get()
+        server_port = self.server_port_entry.get()
+        self.client_name = self.client_name_entry.get()
+
+        ip_regex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
+        if (
+            re.match(ip_regex, server_ip)
+            and int(server_port) > 1024
+            and int(server_port) < 65535
+        ):
+            # L'entrée est valide, on peut continuer à configurer la connexion
+            self.server_ip = server_ip
+            self.server_port = int(server_port)
+            self.log_text.insert(
+                tk.END,
+                f"Connexion à {self.server_ip}:{self.server_port} en tant que {self.client_name}\n",
+            )
+            print("Connecté au serveur")
+            # Connect to the server and send the client's name to register it in the server's database.
+            self.server_socket.connect((self.server_ip, self.server_port))
+            data = json.dumps({"command": "REG", "name": self.client_name}).encode(
+                "utf-8"
+            )
+            self.server_socket.sendall(data)
+            print("Envoi de :", data, " au serveur")
+            response = self.server_socket.recv(1024).decode("utf-8")
+            print("Réponse du serveur : ", response)
+            # self.log_text.insert(tk.END, f"{response}\n")
+            response_data = json.loads(response)
+            if response_data["ack"] == "Registered successfully":
+                self.log_text.insert(tk.END, "Enregistrement réussi\n")
+                self.connexion_serveur = True
+                self.call_button["state"] = tk.NORMAL
+            else:
+                self.log_text.insert(
+                    tk.END,
+                    f"Impossible de s'enregistrer sur le serveur: {response_data['ack']}\n",
+                    "avertissement",
+                )
+
+            # Start listening for incoming call requests in a separate thread.
+            threading.Thread(target=self.listen_for_call_requests, daemon=True).start()
+
+        elif not re.match(ip_regex, server_ip):
+            # L'entrée n'est pas valide, on peut afficher un message d'erreur
+            self.log_text.insert(
+                tk.END, "L'adresse IP du serveur est invalide", "avertissement"
+            )
+
+        elif isinstance(server_port, int):
+            self.log_text.insert(
+                tk.END,
+                "Le numéro de port est invalide, ce doit être un nombre compris entre 1024 et 65535",
+                "avertissement",
+            )
+
+        elif not (int(server_port) > 1024 and int(server_port) < 65535):
+            self.log_text.insert(
+                tk.END,
+                "Le numéro de port doit être compris entre 1024 et 65535",
+                "avertissement",
+            )
+
+    def call(self) -> None:
+        """Initiates a call to the client with the specified name entered in the GUI.
+
+        This method first sends a GET request to the server to retrieve the IP address of the client
+        to be called. Then it sends a START request to the called client, containing the caller's name.
+        """
+
+        print("-----------------")
+        called_client_name = self.call_name_entry.get()
+        if (
+            called_client_name.strip() != ""
+        ):  # https://stackoverflow.com/questions/9573244/how-to-check-if-the-string-is-empty/27982561#27982561
+
+            # Send a GET request to the server to retrieve the IP address of the called client.
+            self.server_socket.sendall(
+                json.dumps({"command": "GET", "name": called_client_name}).encode(
+                    "utf-8"
+                )
+            )
+            response = self.server_socket.recv(1024).decode("utf-8")
+            response_data = json.loads(response)
+            called_client_ip = response_data.get("ip")
+
+            # If the called client's IP was successfully retrieved, send a START request to initiate the call.
+            if called_client_ip != None:
+                self.udp_socket.sendto(
+                    f"START {self.client_name}".encode("utf-8"),
+                    (called_client_ip, 5001),
+                )
+                self.log_text.insert(
+                    tk.END,
+                    f"Appel à {called_client_name} sur {called_client_ip}\n",
+                )
+            else:
+                self.log_text.insert(
+                    tk.END,
+                    f"Impossible d'appeler: {called_client_name} est inconnu de l'annuaire\n",
+                    "avertissement",
+                )
+
+            answer = ""
+            self.udp_socket.settimeout(7)
+            while answer == "":
+                try:
+                    data, addr = self.udp_socket.recvfrom(1024)
+                    answer = data.decode("utf-8")
+                    print(answer)
+                    break
+                # pas de décodage possible si donnée vocale
+                except socket.timeout:
+                    threading.Thread(target=self.listen_for_call_requests())
+                    return
+
+                except Exception as e:
+                    print("décodage impossible", e)
+                    pass
+
+            if answer.startswith("ACCEPT"):
+                self.call_in_progress = True
+                self.transmit_audio(called_client_ip)
+                self.btn_raccrocher["state"] = tk.NORMAL
+
+            elif answer.startswith("REJECT"):
+                threading.Thread(
+                    target=self.listen_for_call_requests, daemon=True
+                ).start()
+        else:
+            self.log_text.insert(
+                tk.END, f"Recipient's name is empty\n", "avertissement"
+            )
+
+    def listen_for_call_requests(self) -> None:
+        """Listens for incoming call requests and opens a new window to accept or reject the call.
+
+        This method runs in a separate thread, continuously listening on port 5001 for incoming call
+        requests. If a call request is received, it opens a new window with accept and reject buttons,
+        and waits for the user to accept or reject the call. If the call is accepted, it sends an ACCEPT
+        message to the caller and starts transmitting audio data.
+        """
+        self.log_text.insert(tk.END, "Mise en écoute\n")
+        self.listening_for_calls = True
+        while self.listening_for_calls:
+            try:
+                data, addr = self.udp_socket.recvfrom(1024)
+                data = data.decode("utf-8")
+            except:
+                pass
+
+            if data.startswith("START"):
+                name = data.split(" ")[1]
+                self.log_text.insert(tk.END, f"Appel reçu de {name}\n")
+                IncomingCallWindow(self, name)
+                self.listening_for_calls = False
+
+    def accept_call(self, caller_name: str) -> None:
+        """Accepts an incoming call request and starts transmitting audio data.
+
+        This method is called when the user clicks the accept button in the incoming call window. It
+        sends an ACCEPT message to the caller, closes the incoming call window, and starts transmitting
+        audio data in a separate thread.
+        """
+        self.listening_for_calls = False
+        self.call_in_progress = True
+        self.log_text.insert(tk.END, f"Call from {caller_name} accepted\n")
+        self.btn_raccrocher["state"] = tk.NORMAL
+
+        # Send an ACCEPT message to the caller.
+        self.server_socket.sendall(
+            json.dumps({"command": "GET", "name": caller_name}).encode("utf-8")
+        )
+        response = self.server_socket.recv(1024).decode("utf-8")
+        response_data = json.loads(response)
+        self.caller_ip = response_data.get("ip")
+
+        # If the caller's IP was successfully retrieved, start transmitting audio data.
+        if self.caller_ip:
+            for _ in range(10):
+                self.udp_socket.sendto("ACCEPT".encode("utf-8"), (self.caller_ip, 5001))
+            self.transmit_audio(self.caller_ip)
+        else:
+            self.log_text.insert(
+                tk.END,
+                f"Error starting call with {caller_name}: {caller_name} does not exist or is offline\n",
+                "avertissement",
+            )
+
+    def reject_call(self, caller_name: str) -> None:
+        """Rejects an incoming call request.
+
+        This method is called when the user clicks the reject button in the incoming call window, or
+        when the incoming call window is closed. It sends a REJECT message to the caller and closes the
+        incoming call window.
+        """
+        self.listening_for_calls = True
+        self.log_text.insert(tk.END, f"Call from {caller_name} rejected\n")
+
+        # Send a REJECT message to the caller.
+        self.server_socket.sendall(
+            json.dumps({"command": "GET", "name": caller_name}).encode("utf-8")
+        )
+        response = self.server_socket.recv(1024).decode("utf-8")
+        response_data = json.loads(response)
+        if "ip" in response_data:
+            self.caller_ip = response_data["ip"]
+            self.udp_socket.sendto("REJECT".encode("utf-8"), (self.caller_ip, 5001))
+        else:
+            self.log_text.insert(
+                tk.END,
+                f"Error rejecting call with {caller_name}: {caller_name} does not exist or is offline\n",
+                "avertissement",
+            )
+
+        self.listen_for_call_requests()
+
+    def transmit_audio(self, caller_ip: str) -> None:
+        """Transmits audio data from the input stream to the recipient's IP address.
+
+        This method is called in a separate thread when a call is accepted. It continuously reads
+        audio data from the input stream and sends it to the recipient's IP address using the UDP
+        socket. It also listens for incoming audio data on port 5001 and plays it through the output
+        stream."""
+
+        self.caller_ip = caller_ip
+
+        def read_audio():
+            while self.call_in_progress:
+                # Read audio data from the input stream and send it to the recipient.
+                data = self.input_stream.read(self.NB_ECHANTILLONS)
+                self.udp_socket.sendto(data, (self.caller_ip, 5001))
+
+        def write_audio():
+            while self.call_in_progress:
+                try:
+                    # Listen for incoming audio data and play it through the output stream.
+                    data, addr = self.udp_socket.recvfrom(self.NB_ECHANTILLONS * 2)
+                    if data.decode("utf-8").startswith("CLOSE"):
+                        self.raccrocher()
+                        return
+                except:
+                    self.output_stream.write(data)
+
+        # Create and start the two threads
+        read_thread = threading.Thread(target=read_audio, daemon=True)
+        write_thread = threading.Thread(target=write_audio, daemon=True)
+        read_thread.start()
+        write_thread.start()
+
+    def raccrocher(self) -> None:
+        """Disconnects from the other client."""
+        if self.call_in_progress:
+            self.call_in_progress = False
+            try:
+                self.input_stream.stop_stream()
+                self.output_stream.stop_stream()
+                self.input_stream.close()
+                self.output_stream.close()
+            except:
+                pass
+            for _ in range(5):
+                self.udp_socket.sendto("CLOSE".encode("utf-8"), (self.caller_ip, 5001))
+            self.log_text.insert(tk.END, "Appel terminé\n")
+            self.btn_raccrocher["state"] = tk.DISABLED
+            threading.Thread(target=self.listen_for_call_requests)
+
+    def close(self) -> None:
+        """Closes the client's GUI window."""
+        self.raccrocher()
+        if self.connexion_serveur:
+            data = json.dumps(
+                {"command": "DISCONNECT", "name": self.client_name}
+            ).encode("utf-8")
+            self.server_socket.sendto(data, (self.server_ip, self.server_port))
+            self.server_socket.close()
+        self.log_text.insert(tk.END, "Déconnexion du serveur\n", "avertissement")
+        self.destroy()
+
+
+class IncomingCallWindow(tk.Toplevel):
+    def __init__(self, c: Client, name):
+        super().__init__(c.master)
+        self.name = name
+        self.c = c
+
+        self.title(f"Incoming call from {name}")
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
+
+        self.call_label = tk.Label(self, text=f"Incoming call from {name}")
+        self.call_label.pack(side="top")
+
+        self.accept_button = tk.Button(
+            self, text="Accept", command=self.accept, bg="green"
+        )
+        self.accept_button.pack(side="right", anchor="s")
+
+        self.decline_button = tk.Button(
+            self, text="Decline", command=self.decline, bg="red"
+        )
+        self.decline_button.pack(side="left", anchor="s")
+
+    def accept(self):
+        self.c.accept_call(self.name)
+        self.destroy()
+
+    def decline(self):
+        self.c.reject_call(self.name)
+        self.destroy()
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.mainloop()
---
+++ python_serv.py
@@ -0,0 +1,231 @@
+import socket
+import json
+import sqlite3
+import threading
+from typing import List, Tuple
+import time
+import sys
+
+from pprint import pprint
+
+from tkinter import *
+from tkinter import ttk
+
+
+class Server:
+    def __init__(self, master: Tk, address: str, port: int):
+
+        self.threads = []
+        self.run_thread = True
+
+        self.master = master
+        self.address = address
+        self.port = port
+
+        self.log_area = Text(self.master)
+
+        try:
+            self.db = sqlite3.connect("bdd.sqlite", check_same_thread=False)
+            self.cursor = self.db.cursor()
+            self.cursor.execute(
+                "CREATE TABLE IF NOT EXISTS users(username TEXT, ip TEXT)"
+            )
+
+            # On vide la table avant de commencer
+            self.cursor.execute("DELETE FROM users")
+            self.db.commit()
+            self.log_area.insert(
+                END,
+                "Connecté à la BDD\n",
+            )
+
+        except:
+            self.log_area.insert(
+                END,
+                "Connexion à la BDD échouée\n",
+            )
+
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.listener.bind((self.address, self.port))
+            self.listener.listen(5)
+            self.log_area.insert(
+                END,
+                f"En écoute sur {self.address}:{self.port}\n",
+            )
+        except socket.error as e:
+            print(f"Error binding to address {self.address} and port {self.port}: {e}")
+            self.log_area.insert(
+                END,
+                f"Erreur pour se lier à {self.address}:{self.port}: {e}\n",
+            )
+            self.listener.close()
+
+        thread = threading.Thread(target=self.accept_connections, daemon=True)
+        thread.start()
+        self.log_area.insert(
+            END,
+            "Lancement Thread accept_connections\n",
+        )
+        self.threads.append(thread)
+
+        self.label = ttk.Label(
+            self.master, text=f"Listening on {self.address}:{self.port}"
+        )
+        self.label.pack()
+        self.log_area.pack()
+
+        self.close_button = ttk.Button(self.master, text="Close", command=self.close)
+        self.close_button.pack()
+
+        # Add a small frame on the server window to live view the content of the database
+
+        self.db_frame = ttk.Frame(self.master)
+        self.db_frame.pack()
+
+        # Add a Treeview widget to the frame and bind it to a scrollbar
+        self.db_tree = ttk.Treeview(self.db_frame, height=5)
+        self.db_scroll = ttk.Scrollbar(
+            self.db_frame, orient="vertical", command=self.db_tree.yview
+        )
+        self.db_tree.configure(yscrollcommand=self.db_scroll.set)
+
+        # Set the columns of the Treeview widget
+        self.db_tree["columns"] = "ip"
+        self.db_tree.column("#0", width=100, minwidth=100)
+        self.db_tree.column("ip", width=100, minwidth=100)
+        self.db_tree.heading("#0", text="Name")
+        self.db_tree.heading("ip", text="IP")
+
+        # Pack the Treeview widget and the scrollbar
+        self.db_tree.pack(side="left")
+        self.db_scroll.pack(side="right", fill="y")
+
+        # Update the Treeview widget with the content of the database in a separate thread
+        thread2 = threading.Thread(target=self.update_db_tree)
+        thread2.start()
+        self.threads.append(thread2)
+
+    def update_db_tree(self):
+        while self.run_thread:
+            # Clear the Treeview widget
+            for i in self.db_tree.get_children():
+                self.db_tree.delete(i)
+
+            # Retrieve the content of the database
+            self.cursor.execute("SELECT * FROM users")
+            rows = self.cursor.fetchall()
+
+            # Add the rows to the Treeview widget
+            for row in rows:
+                self.db_tree.insert("", "end", text=row[0], values=(row[1],))
+
+            # Sleep for 1 second before updating the Treeview widget again
+            time.sleep(1)
+
+    def accept_connections(self):
+        while self.run_thread:
+            try:
+                client, address = self.listener.accept()
+                # Create a new thread for each client connection and start the thread.
+                thread = threading.Thread(
+                    target=self.handle_client, args=(client, address)
+                )
+                thread.start()
+                self.threads.append(thread)
+            except Exception as e:
+                pass
+
+    def delete_user(self, name: str):
+        print(name)
+        self.cursor.execute(f'DELETE FROM users WHERE username="{name}"')
+        self.db.commit()
+
+    def handle_client(self, client: socket.socket, address: Tuple[str, int]):
+        try:
+            while self.run_thread:
+                data = client.recv(1024)
+                data = json.loads(data.decode())
+
+                if data["command"] == "REG":
+                    try:
+                        response = self.register_user(data["name"], address[0])
+                        if response:
+                            response = {"ack": "Registered successfully"}
+                            self.log_area.insert(
+                                END,
+                                f"Registered user {data['name']} with IP {address[0]}\n",
+                            )
+                        else:
+                            response = {"ack": "Error Registering"}
+                            self.log_area.insert(
+                                END,
+                                f"User {data['name']} already registered with IP {address[0]}\n",
+                            )
+                    except Exception as e:
+                        response = {"ack": "Error"}
+                        self.log_area.insert(
+                            END, f"Error registering user {data['name']} : {e} \n"
+                        )
+                elif data["command"] == "GET":
+                    result = self.get_ip(data["name"])
+                    if result is None:
+                        response = {"ip": None}
+                    else:
+                        response = {"ip": result}
+
+                elif data["command"] == "DISCONNECT":
+                    self.delete_user(data["name"])
+
+                client.send(json.dumps(response).encode())
+
+        except:
+            try:
+                # Handle any exceptions that may occur while handling the client's request
+                self.delete_user(data["name"])
+            except:
+                pass
+            self.log_area.insert(END, f"{data['name']} disconnected\n")
+
+    def register_user(self, name: str, ip: str):
+        self.cursor.execute(f'SELECT * FROM users WHERE username="{name}"')
+        result = self.cursor.fetchone()
+
+        if result is not None:
+            return False
+
+        self.cursor.execute("INSERT INTO users(username, ip) VALUES (?, ?)", (name, ip))
+        self.db.commit()
+        return True
+
+    def get_ip(self, name: str):
+        self.cursor.execute("SELECT * FROM users WHERE username=?", (name,))
+        result = self.cursor.fetchone()
+
+        print(result)
+
+        if result is None:
+            return None
+
+        return result[1]
+
+    def close(self):
+        # Disable new incoming connections
+        self.run_thread = False
+        if self.listener:
+            self.listener.close()
+        self.db.close()
+        self.master.destroy()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Server")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    serv_adress = s.getsockname()[0]
+
+    server = Server(root, serv_adress, 10000)
+    root.mainloop()
